7a40ee6cf9cbeb67869919dcf4cf7a31223395d2
yoonminsang <apdlyooapdl@naver.com>
2021-02-08 23:45:57 +0900

    ex

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Counter from "./Counter";
+import Info from "./Info";
+import Average from "./Average";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  // const [visible, setVisible] = useState(false);
+  // return (
+  //   <div>
+  //     <button
+  //       onClick={() => {
+  //         setVisible(!visible);
+  //       }}
+  //     >
+  //       {visible ? "숨기기" : "보이기"}
+  //     </button>
+  //     <hr />
+  //     {visible && <Info />}
+  //   </div>
+  // );
+  // return <Average />;
+  return <Info />;
+  // return <Counter />;
+};
 
 export default App;
---
+++ src/RefSample.js
@@ -0,0 +1,14 @@
+import React, { useRef } from "react";
+
+const RefSample = () => {
+  const id = useRef(1);
+  const setId = (n) => {
+    id.current = n;
+  };
+  const printId = () => {
+    console.log(id.current);
+  };
+  return <div>refsample</div>;
+};
+
+export default RefSample;
